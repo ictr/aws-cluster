76f60af067041f199898046516793ef85057ec49
Bo Peng <ben.bob@gmail.com>
2021-04-12 16:33:14 -0500

    Update pre-install script

--- pre-install.sh
+++ pre-install.sh
@@ -14,16 +14,19 @@ wget http://www.well.ox.ac.uk/~gav/resources/snptest_v2.5.2_CentOS6.5_x86_64_sta
 ## gcta
 wget https://cnsgenomics.com/software/gcta/bin/gcta_1.93.2beta.zip && \
     unzip gcta_1.93.2beta.zip && \
-    mv gcta_1.93.2beta/gcta64 && /usr/local/bin/gcta64
+    mv gcta_1.93.2beta/gcta64 /usr/local/bin/gcta64
 
-## qctool, need to get a newer version of libstdc++
+## qctool
+
+# need to get a newer version of libstdc++
 wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh && \
     bash Anaconda3-2019.07-Linux-x86_64.sh -b && \
-    cp /home/centos/anaconda3/lib/libstdc++.so.6.0.26 /usr/lib64 && \
+    cp /root/anaconda3/lib/libstdc++.so.6.0.26 /usr/lib64 && \
     rm /usr/lib64/libstdc++.so.6 && \
     ln -s /usr/lib64/libstdc++.so.6.0.26 /usr/lib64/libstdc++.so.6
 
-# upgrade libstdc++
+yum install -y lapack
+
 wget https://www.well.ox.ac.uk/~gav/resources/qctool_v2.0.8-CentOS_Linux7.6.1810-x86_64.tgz && \
     tar zxf qctool_v2.0.8-CentOS_Linux7.6.1810-x86_64.tgz && \
     mv qctool_v2.0.8-CentOS\ Linux7.6.1810-x86_64/qctool /usr/local/bin
