213cad09af825e100a766c93c28c4281c3d38d08
moorthyleela12 <66921290+moorthyleela12@users.noreply.github.com>
2021-08-05 13:39:17 -0500

    Update pre-install-plink-shapeit-beagle.sh

--- pre-install-plink-shapeit-beagle.sh
+++ pre-install-plink-shapeit-beagle.sh
@@ -4,10 +4,10 @@
 
 ## singularity
 
-yum update -y && \
-    yum install -y epel-release && \
-    yum update -y && \
-    yum install -y singularity
+#yum update -y && \
+   # yum install -y epel-release && \
+    #yum update -y && \
+    #yum install -y singularity
 
 
 ## plink
@@ -20,5 +20,5 @@ wget http://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20201019.zip && \
 
 
 ## beagle 4
-wget https://faculty.washington.edu/browning/beagle/beagle.r1399.jar && \
-    mv -f beagle.r1399.jar /usr/local/bin/
+#wget https://faculty.washington.edu/browning/beagle/beagle.r1399.jar && \
+ #   mv -f beagle.r1399.jar /usr/local/bin/
